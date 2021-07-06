7ca601257aed8dd297add238017b12446eafd1af
fawkesito <85775920+fawkesito@users.noreply.github.com>
2021-07-06 14:30:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu-btn").addEventListener("click", () =>{
+     document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.anime-cards', {delay: 100});
+ScrollReveal().reveal('.cards-banner-one', {delay: 100});
+
+
+
